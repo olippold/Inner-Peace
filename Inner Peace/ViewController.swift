59a60e88f0f4f1381dfa65f004c155b75487353a
Oliver Lippold <olippold@hotmail.com>
2020-07-18 13:49:37 +0100

    Initial Commit

---
+++ Inner Peace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inner Peace
+//
+//  Created by Oliver Lippold on 18/07/2020.
+//  Copyright © 2020 Oliver Lippold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
